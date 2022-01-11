19c9b78b123bd331479d39ee4c68e051aa8fba0d
Jyuri <jeon1310@gmail.com>
2022-01-11 18:31:00 +0900

    레이아웃 구성 완료

---
+++ app/src/main/java/kr/hs/emirim/w2015/pickone/ExplanPageActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.w2015.pickone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExplanPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explan_page)
+    }
+}
